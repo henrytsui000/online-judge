46a8d0c3538bc1c6a94ea6e63f7cb390655d5633
henrytsui <henrytsui000@gmail.com>
2020-07-27 18:12:11 +0800

    upd bomb

--- bomp/pomp.cpp
+++ bomp/pomp.cpp
@@ -50,7 +50,7 @@ ll mypow(ll a,ll b){
 
 /******good luck******/
 const int maxxn = 80;
-int maxn,bomb,flag_cnt;
+int maxn,bomb,flag_cnt,t_move;
 bool die=false;
 char arr[maxxn][maxxn];
 char ans[maxxn][maxxn];
@@ -79,7 +79,6 @@ int count_bomb(int x,int y){
 
 void give_map(){
     int cnt = bomb;
-
     rep(i,maxn-1)
         ans[i][0]=ans[0][i]=ans[i][maxn-1]=ans[maxn-1][i]='A'+i-1;
     ans[0][0]=ans[0][maxn-1]=ans[maxn-1][0]=ans[maxn-1][maxn-1]='@';
@@ -117,33 +116,38 @@ void help(){
         <<"use show to see the map right now"<<endl;
 }
 
-int open_counter(){
-    int ret = 0;
-    rep1(i,maxn-1){
-        rep1(j,maxn-1){
+int open_counter(int t){
+    if(t==1){
+        int ret = 0;
+        rep1(i,maxn-1) rep1(j,maxn-1)
             if(arr[i][j]!='$'&&arr[i][j]!='F')
-                ret++;
-        }
+                    ret++;
+        return ret;
+    }else if(t==2){
+        int ret = 0;
+        rep1(i,maxn-1) rep1(j,maxn-1)
+            if(arr[i][j]=='F')
+                    ret++;
+        return ret;
+
     }
-    return ret;
 }
 
-int count_flag(int x,int y){
+bool count_flag(int x,int y){
     int ret=0;
     rep(i,9) if(i!=4) 
         if(arr[x+dir[i].F][y+dir[i].S]=='F')
             ret++;
-    return ret;
+    if(arr[x][y]-ret=='0')
+        return true;
+    else return false;
 }
 
 void step(int x,int y){
     if(x*y==0||x==maxn-1||y==maxn-1||arr[x][y]=='F')
         return ;
-    int flg=count_flag(x,y);
-    if(ans[x][y]=='X'){
-        die=1;
-        return ;
-    }else if(ans[x][y]=='0'&&arr[x][y]=='$'){
+    if(ans[x][y]=='X'&&(die=1)) return ;
+    else if(ans[x][y]=='0'&&arr[x][y]=='$'){
         arr[x][y]=ans[x][y];
         rep(i,9) if(nbdy(x,y)) 
             step(x+dir[i].F,y+dir[i].S);
@@ -152,14 +156,23 @@ void step(int x,int y){
 
 void extra_step(int x,int y){
     rep(i,9) if(nbdy(x,y)){
-        if(arr[x][y]=='X'){
-            die=1;
-            return ;
-        }else if(arr[x][y]!='F')arr[x][y]=ans[x][y];
+        if(arr[x+dir[i].F][y+dir[i].S]!='F'){
+            if(ans[x+dir[i].F][y+dir[i].S]=='X'&&(die=1)) return ;
+            arr[x+dir[i].F][y+dir[i].S]=ans[x+dir[i].F][y+dir[i].S];
+        }
     }
 }
 
+void show_detail(){
+    cout<<"total bomb: "<<bomb<<endl;
+    cout<<"total flag: "<<open_counter(2)<<endl;
+    cout<<"remaining bombs: "<<bomb-open_counter(2)<<endl;
+    cout<<"opening space: "<<open_counter(1)<<endl;
+    cout<<"total move: "<<t_move<<endl;
+}
+
 void start(){
+    char x,y;
     t_start=time(NULL);
     cout<<"the game have been start"<<endl
         <<"u can type help for help"<<endl;
@@ -167,23 +180,23 @@ void start(){
     cout<<">>>";
     while(cin>>str){
         if(str=="flag"){
-            char x,y;
             cin>>x>>y;
             arr[p(x)][p(y)]='F';
         }else if(str=="step"){
-            char x,y;
             cin>>x>>y;
+            while(!t_move&&ans[p(x)][p(y)]=='X'){
+                give_map();
+            }
+            t_move++;
             if(arr[p(x)][p(y)]=='F')
                 cout<<"you can't step flag point"<<endl;
-            else if(arr[x][y]<'9'&&arr[x][y]>='0'){
+            else if(arr[p(x)][p(y)]<'9'&&arr[p(x)][p(y)]>='0'&&count_flag(p(x),p(y))){
                 extra_step(p(x),p(y));
             }else step(p(x),p(y));
         }else if(str=="unknow"){
-            char x,y;
             cin>>x>>y;
-            arr[p(x)][p(y)]='U';
+            arr[p(x)][p(y)]='?';
         }else if(str=="unflag"){
-            char x,y;
             cin>>x>>y;
             arr[p(x)][p(y)]='$';
         }else if(str=="show"){
@@ -194,7 +207,9 @@ void start(){
                 cout<<endl;
             }
         }else if(str=="detail"){
-            cout<<time(NULL)-t_start<<endl;
+            cout<<"time: "<<(time(NULL)-t_start)/60<<"min "
+                <<(time(NULL)-t_start)%60<<"sec"<<endl;
+            show_detail();
         }else if(str=="help"){
             help();
         }else if(str=="ans"){
@@ -205,19 +220,27 @@ void start(){
                 cout<<endl;
             }
         }else if(str=="muti-step"){
-            char x,y;
             while(cin>>x>>y){
+                if(x=='-'&&y=='-')
+                    break;
+                t_move++;
                 if(arr[p(x)][p(y)]=='F')
                     cout<<"you can't step flag point"<<endl;
-                else if(arr[x][y]<'9'&&arr[x][y]>='0'){
+                else if(arr[p(x)][p(y)]<'9'&&arr[p(x)][p(y)]>='0'&&count_flag(p(x),p(y))){
                     extra_step(p(x),p(y));
                 }else step(p(x),p(y));
+            }
+        }else if(str=="muti-flag"){
+            while(cin>>x>>y){
+                if(x=='-'&&y=='-')
+                    break;
+                arr[p(x)][p(y)]='F';
             }
         }else{
             cout<<"unknown choice"<<endl;
         }
         if(die) break;
-        else if((maxn-2)*(maxn-2)==bomb+open_counter()) break;
+        else if((maxn-2)*(maxn-2)==bomb+open_counter(1)) break;
         cout<<">>>";
     }
     if(die){
