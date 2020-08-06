244352b7d76e4a383b96a41cc1f0af83129a33cb
henrytsui <henrytsui000@gmail.com>
2020-08-06 12:10:49 +0800

    upd 0806

--- stu.cpp
+++ stu.cpp
@@ -1,25 +1,83 @@
 #include <bits/stdc++.h>
+#pragma GCC optimize("unroll-loops,no-stack-protector")
 using namespace std;
 using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using pdd = pair<ld,ld>;
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0)
 #define endl '\n'
 #define all(a) a.begin(),a.end()
 #define sz(a) ((int)a.size())
+#define F first
+#define S second
 #define rep(i,n) for(int i=0;i<(int)n;i++)
-#define rep1(i,n) for(int i=1;i<(int)n;i++)
+#define rep1(i,n) for(int i=1;i<=(int)n;i++)
+#define pb push_back
 #define eb emplace_back
+#define mp(a,b) make_pair(a,b)
+#define cans cout<<ans<<endl
+#define in cout<<"in lar"<<endl
+#define SORT_UNIQUE(c) (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+#define GET_POS(c,x) (lower_bound(c.begin(),c.end(),x)-c.begin())
+template<typename T1,typename T2>
+ostream& operator<<(ostream& out,pair<T1,T2> P){
+    out<<'('<<P.F<<','<<P.S<<')';
+    return out;
+}
+
+pii operator-(pii a,pii b){
+    pii ret;
+    ret.F=a.F-b.F;
+    ret.S=a.S-b.F;
+    return ret;
+}
+
+int operator*(pii a,pii b){
+    if(a.F*b.S-a.S*b.F)
+        return -1
+
+}
+
+//}}}
+const ll INF64=8000000000000000000LL;
+const int INF=0x3f3f3f3f;
+const ll MOD=ll(1e9+7);
+const ld PI=acos(-1);
+const ld eps=1e-9;
+//const ll p=880301;
+//const ll P=31;
+
+ll mypow(ll a,ll b){
+    ll res=1LL;
+    while(b){
+        if(b&1) res=res*a%MOD;
+        a=a*a%MOD;
+        b>>=1;
+    }
+    return res;
+}
+
 const int maxn = 1e5+5;
-int arr[maxn];
-int dp[maxn];
+pii con[maxn];
+
+vector<int> up;
+vector<int> dw;
+
+int cal(pii a,pii b,pii c){
+    pii p_1=b-a,p_2=c-a;
+    cout<<p_1<<endl;
+    cout<<p_2<<endl;
+    if(p_1*p_2>0) return 1;
+    else return 0;
+}
+
 int32_t main(){
-    int n,k;
-    cin>>n>>k;
-    k++;
-    rep(i,n) cin>>arr[i];
-    rep1(i,k)
-        dp[i]=max(dp[i-1],arr[i]);
-    rep(i,n) if(i-k>=0)
-            dp[i]=max(dp[i-k]+arr[i],dp[i-1]);
-    cout<<dp[n-1]<<endl;
+    int n;
+    rep(i,n)
+        cin>>con[i].F>>con[i].S;
+    sort(con,con+n);
+
     return 0;
 }
