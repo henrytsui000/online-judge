2ffdc0a8686abacc56ebf1ae94a3b9bdd385670f
henrytsui <henrytsui000@gmail.com>
2020-09-21 16:01:01 +0800

    dij

--- trying_code/dijstra.cpp
+++ trying_code/dijstra.cpp
@@ -15,13 +15,19 @@ using pii = pair<int, int>;
 #define mp make_pair
 const int maxn = 1e5 + 5;
 
+template <typename T1, typename T2>
+ostream& operator<<(ostream& out, pair<T1, T2> P) {
+    out << '(' << P.X << ',' << P.Y << ')';
+    return out;
+}
+
 bool cmp(pii x, pii y) { return x.Y < y.Y; }
 
 int arr[maxn];
 int node[maxn];
 
 vector<pii> edge[maxn];
-priority_queue<pii> pri;
+priority_queue<pii,vector<pii>,greater<pii>>pri,t;
 
 int dsu[maxn], siz[maxn];
 bool vis[maxn];
@@ -29,25 +35,39 @@ int n;
 
 void init() { rep(i, n) dsu[i] = i, siz[i] = 1; }
 
-
 void dfs(int np) {
+    // cout<<np<<' ';
     vis[np] = 1;
-    for (pii x : edge[np])
-        if (!vis[x.Y]) {
-            pri.push(mp(node[np] + x.Y, x.X));
+    for (pii x : edge[np]){
+        // cout<<np<<' '<< x.X<<' '<< x.Y<<endl;
+        if (!vis[x.X]) {
+            pri.push({node[np] + x.Y, x.X});
         }
+    }
     while (!pri.empty()) {
         pii p = pri.top();
         pri.pop();
         if (!vis[p.Y]) {
-            node[p.Y] = pri.top().X;
+            node[p.Y] = p.X;
             dfs(p.Y);
             return;
         }
     }
 }
 
+
+void test(){
+    rep(i,10)
+        t.push({10-i,i});
+    while(!t.empty()){
+        cout<<(t.top().X)<<' ';
+        t.pop();
+    }
+    cout<<endl;
+}
+
 int32_t main() {
+    // test();
     ifstream fin("test.txt");
     int n, m;
     fin >> n >> m;
@@ -57,7 +77,6 @@ int32_t main() {
         edge[u].eb(mp(v, x));
     }
     dfs(0);
-    rep(i,n)
-        cout<<node[i]<<" \n"[n-1==i];
+    rep(i, n) cout << node[i] << " \n"[n - 1 == i];
     return 0;
 }
