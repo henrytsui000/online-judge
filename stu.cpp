9ca99ac127725142cfe1ffecfc2f73a313ad7119
henrytsui000 <henrytsui000@gmail.com>
2021-02-05 00:23:26 +0800

    pi

--- stu.cpp
+++ stu.cpp
@@ -1,60 +1,9 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-#define IOS                       \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0)
-#define endl '\n'
-#define all(a) a.begin(), a.end()
-#define sz(a) ((int)a.size())
-#define rep(i, n) for (int i = 0; i < (int)n; i++)
-#define eb emplace_back
-const int maxn = 1e5 + 5;
-map<string, char> stc;
-void init() {
-    stc[".-"] = 'A';
-    stc["-..."] = 'B';
-    stc["-.-."] = 'C';
-    stc["-.."] = 'D';
-    stc["."] = 'E';
-    stc["..-."] = 'F';
-    stc["--."] = 'G';
-    stc["...."] = 'H';
-    stc[".."] = 'I';
-    stc[".---"] = 'J';
-    stc["-.-"] = 'K';
-    stc[".-.."] = 'L';
-    stc["--"] = 'M';
-    stc["-."] = 'N';
-    stc["---"] = 'O';
-    stc[".--."] = 'P';
-    stc["--.-"] = 'Q';
-    stc[".-."] = 'R';
-    stc["..."] = 'S';
-    stc["-"] = 'T';
-    stc["..-"] = 'U';
-    stc["...-"] = 'V';
-    stc[".--"] = 'W';
-    stc["-..-"] = 'X';
-    stc["-.--"] = 'Y';
-    stc["--.."] = 'Z';
-}
-
-int32_t main() {
-    init();
-    // IOS;
-    int t;
-    cin >> t;
-    while (t--) {
-        string str;
-        char ch;
-        bool key = true;
-        while (key && cin >> str) {
-            cout << stc[str];
-            ch = getchar();
-            if (ch == 10) key = 0;
-        }
-        cout << endl;
-    }
+#include<bits/stdc++.h>
+using namespcae std;
+int main(){
+    system("a.out > a.txt");
+    ststem("b.out > b.txt");
+    if(system("fc a.txt b.txt"))
+        cout << "fuck" <<endl;
     return 0;
 }
--- trying_code/ascii.cpp
+++ trying_code/ascii.cpp
@@ -1,56 +1,28 @@
 #include <bits/stdc++.h>
-#pragma GCC optimize("unroll-loops,no-stack-protector")
 using namespace std;
 using ll = long long;
-using ld = long double;
-using pii = pair<int,int>;
-using pll = pair<ll,ll>;
-using pdd = pair<ld,ld>;
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0)
+#define IOS                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0)
 #define endl '\n'
-#define all(a) a.begin(),a.end()
+#define all(a) a.begin(), a.end()
 #define sz(a) ((int)a.size())
-#define F first
-#define S second
-#define rep(i,n) for(int i=0;i<(int)n;i++)
-#define rep1(i,n) for(int i=1;i<(int)n;i++)
-#define pb push_back
+#define rep(i, n) for (int i = 0; i < (int)n; i++)
 #define eb emplace_back
-#define mp(a,b) make_pair(a,b)
-#define cans cout<<ans<<endl
-#define in cout<<"in lar"<<endl
-#define sort_uni(c) (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
-#define get(c,x) (lower_bound(c.begin(),c.end(),x)-c.begin())
-template<typename T1,typename T2>
-ostream& operator<<(ostream& out,pair<T1,T2> P){
-    out<<'('<<P.F<<','<<P.S<<')';
-    return out;
-}
+const int maxn = 1e5 + 5;
+using ld = long double;
+const ld PI = acos(-1);
 
-//}}}
-const ll INF64=8000000000000000000LL;
-const int INF=0x3f3f3f3f;
-const ll MOD=ll(1e9+7);
-const ld PI=acos(-1);
-const ld eps=1e-9;
-//const ll p=880301;
-//const ll P=31;
-
-ll mypow(ll a,ll b){
-    ll res=1LL;
-    while(b){
-        if(b&1) res=res*a%MOD;
-        a=a*a%MOD;
-        b>>=1;
+int32_t main() {
+    int a, chi;
+    cin >> a >> chi;
+    if (chi <= 180) {
+        cout << "safe" << endl;
+        return 0;
     }
-    return res;
-}
-
-/******good luck******/
-
-int32_t main(){
-    IOS;
-    char ch=70;
-    cout<<(char)70<<endl;
+    if (chi > 180) chi = 360 - chi;
+    if (chi > 90) chi = 180 - chi;
+    ld result = sin(chi * PI / 180);
+    cout << int(a / result + 1e-6) << endl;
     return 0;
 }
