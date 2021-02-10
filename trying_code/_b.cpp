6726cb382bbe7a0e038fe8d40b82c96869867447
henrytsui <henrytsui000@gmail.com>
2021-02-11 01:53:54 +0800

    clab test

---
+++ trying_code/_b.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b, c = 0;
+    cin >> a >> b;
+    c = a;
+    for(int i = 0; i < b; i++)c++;
+    cout<<c<<endl;
+    return 0;
+}
