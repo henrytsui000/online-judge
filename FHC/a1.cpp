a327e690799f3adbfbc82b3d5ec1bd165dcf596b
henrytsui000 <henrytsui000@gmail.com>
2022-02-02 11:06:38 +0800

    i forgot

--- FHC/a1.cpp
+++ FHC/a1.cpp
@@ -1,44 +1,209 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define rep(i,n) for(int i = 0 ; i < (int)n; i++)
-#define sz(a) ((int)a.size())
+#include "wave.h"
+#include "ui_wave.h"
+#include <QDebug>
+#include <QFileDialog>
+#define cout qDebug()
 
-void solve() {
-    /* todo */
-    string str;
-    cin >> str;
-    set<char> s = {'A', 'E', 'I', 'O', 'U'};
-    map<char, int> mp;
-    rep(i,sz(str))
-        mp[str[i]]++;
-    int same = 0;
-    rep(i, sz(str))
-        if(str[i]!=str[0])
-            same = 1;
-    int ans = 1e9*same;
-    
-    rep(i, 26){
-        int cnt = 0;
-        rep(j, 26){
-            if(i != j)
-                cnt += (2-(s.count('A'+j)^s.count('A'+i)))*mp['A'+j];
-        }
-        ans = min(cnt, ans);
-    }
-    cout << ans << "\n";
+Wave::Wave(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Wave)
+{
+    ui->setupUi(this);
+    qDebug() << "check";
+    scene = new QGraphicsScene(0,0,800, 600);
+//    this->setScreen(scene);
+    ui->graphicsView->setScene(scene);
+    pixmap = new QPixmap(800, 600);
+    painter = new QPainter(pixmap);
+    painter->fillRect(0,0,800,600, Qt::white);
+    itempixmap = new QGraphicsPixmapItem(*pixmap);
+    scene->addItem(itempixmap);
+//define aimer2
+    aimer[0] = new QGraphicsLineItem();
+    aimer[1] = new QGraphicsLineItem();
+    aimer[2] = new QGraphicsLineItem();
+    pen = new QPen();
+    pen->setStyle(Qt::DashLine);
+    pen->setWidth(1);
+    pen->setColor(Qt::black);
+    aimer[0] = scene->addLine( 0, 0, 70, 70, *pen);
+    aimer[2] = scene->addLine( 0, 70, 0, 70, *pen);
+    pen->setColor(Qt::red);
+    pen->setWidth(10);
+    aimer[1] = scene->addLine(1000, 1000, 1000, 1000, *pen);
+//    aimer[0]->setLine(0,20, 10000, 20);
+
+    aimer[0]->setVisible(cur);
+    aimer[1]->setVisible(cur);
+    aimer[2]->setVisible(cur);
+
 }
 
-int main() {
-    freopen("consistency_chapter_1_validation_input.txt", "r", stdin);
-    freopen("A.out", "w", stdout);
-    
-    int T;
-    cin >> T;
-    
-    for (int test = 1; test <= T; ++test) {
-        cout << "Case #" << test << ": ";
-        solve();
+Wave::~Wave()
+{
+    delete ui;
+}
+
+void Wave::gogo(){
+    cout << "MASD";
+}
+
+//show//
+void Wave::on_pushButton_clicked() {
+    qDebug() << "deb";
+    qDebug() << circuit->all_IC[0]->value;
+    Stime = std::max(1e-12, Stime);
+    qDebug() << "my st" << Stime;
+    for(auto x: circuit->all_node){
+        for(auto V: x->Voltage)
+            cout << V.real() << V.imag();
+        cout << ' ';
     }
-    
-    return 0;
+
+    QSet<Node*> tmp, nw;
+    qDebug() << "wave_node: " << circuit->wave_node.size();
+    for(auto x: circuit->wave_node){
+        if(!tmp.contains(circuit->rela[x])){
+            nw.insert(x);
+            tmp.insert(circuit->rela[x]);
+        }
+        for(auto V: x->Voltage)
+                cout << V.real() << V.imag();
+    }
+    circuit->wave_node = nw;
+    qDebug() << "wave_node n: " << circuit->wave_node.size();
+    Vmax = -1e6, Vmin = 1e6;
+    int r = 0;
+
+    QVector<IC*> vs;
+    for(auto x: circuit->all_IC){
+        if(x->type == "V"){
+            vs.push_back(x);
+        }
+    }
+    for(int i=0; i<100; i++){
+        vec[i].clear();
+    }
+    for(auto &x: vec)
+        for(int i = 0 ; i <= 801; i++)
+            x.push_back(0);
+    for(auto x: circuit->wave_node){
+        for(int v_idx = 0 ; v_idx < x->Voltage.size(); v_idx++){
+            double R = x->Voltage[v_idx].real(), I = x->Voltage[v_idx].imag();
+            qDebug()<<v_idx<<R<<I;
+            double c = sqrt(R*R + I*I), d = atan(I/(R+1e-6));
+            double step = Stime/800, nt = 0, Vt;
+            int idx = 0;
+            cout << "CD" << c << d;
+            qDebug()<<"is_ac:"<<vs[v_idx]->wave_type;
+            while(nt <= Stime){
+                if(vs[v_idx]->wave_type=="AC")
+                    Vt = c*cos((x->Frequen[v_idx])*2*M_PI*nt+d);
+                else
+                    Vt = c*cos((x->Frequen[v_idx])*2*M_PI*nt+d+M_PI/2);
+                vec[r][idx]+=Vt;
+                //qDebug() <<"Vt" << idx<<Vt;
+                Vmax=std::max(vec[r][idx], Vmax);
+                Vmin=std::min(vec[r][idx], Vmin);
+                idx++;
+                nt+=step;
+            }
+            qDebug() <<  x->Voltage[v_idx].real();
+        }
+        r++;
+    }
+    qDebug() << "VMAXVMIN" << Vmax << Vmin;
+    Vm = (Vmax+Vmin)/2;
+    HOR.clear();
+
+    pen->setStyle(Qt::SolidLine);
+    pen->setWidth(1);
+    ui->label->setText(QString::number(Vmax, 'f', 3));
+    ui->label_2->setText(QString::number(Vm, 'f', 3));
+    ui->label_3->setText(QString::number(Vmin, 'f', 3));
+    ui->label_4->setText(QString::number(0, 'f', 3));
+    ui->label_5->setText(QString::number(Stime, 'f', 3));
+    for(int i = 0 ; i < circuit->wave_node.size(); i++){
+        pen->setColor(col[i%10]);
+        for(int j=0; j<vec[i].size()-1; j++){
+            //qDebug() << 'S' << i << j << vec[i][j];
+            scene->addLine(j, 300-(vec[i][j]-Vm)/(Vmax-Vm+1e-6)*290, j+1, 300-(vec[i][j+1]-Vm)/(Vmax-Vm+1e-6)*290, *pen);
+        }
+    }
+//    itempixmap = new QGraphicsPixmapItem(*pixmap);
+//    scene->addItem(itempixmap);
+    siz = circuit->wave_node.size();
+}
+
+//save//
+void Wave::on_pushButton_2_clicked() {
+    QString f_str = QFileDialog::getSaveFileName(this, "Save","C:\\", "All Files (*.csv)");
+    QFile saveFile(f_str);
+    if(!saveFile.open(QIODevice::WriteOnly)){
+        qDebug() << "Cannot save file: " << f_str;
+        return;
+    }
+    QDataStream saveStream(&saveFile);
+    saveStream << "T ,";
+    for(int j = 0 ; j < siz ; j++){
+        saveStream << QString::number(j+1) << " ,";
+    }
+    saveStream << "\n";
+    for(int i = 0 ; i <= 800; i ++){
+        QString tmp = QString::number(i) + " ,";
+        for(int j = 0 ; j < siz ; j++){
+//            qDebug() << i << j  << vec[j][i];
+            tmp += QString::number(vec[j][i]) + " ,";
+        }
+        tmp+="\n";
+        cout << tmp;
+        saveStream << tmp;
+    }
+    saveFile.close();
+}
+
+//curser//
+void Wave::mouseMoveEvent(QMouseEvent *event){
+    cout << event->x() << event->y();
+    pen->setStyle(Qt::DashLine);
+    pen->setColor(Qt::black);
+    int idx = std::max(std::min(900, event->x()), 100);
+    aimer[0]->setLine(event->x()-100, 0, event->x()-100, 10000);
+//    scene->addItem(aimer[0]);
+//    QPixmap* pixmap2 = pixmap;
+//    QPainter* painter2 = new QPainter(pixmap2);
+//    QPen penr(Qt::red, 1);
+//    painter2->setPen(penr);
+//    painter2->drawLine(event->x()-100, 0, event->x()-100, 10000);
+//    itempixmap = new QGraphicsPixmapItem(*pixmap2);
+//    scene->addItem(itempixmap);
+    if(ui->comboBox->count()){
+        int sit =  300-(vec[cur_idx][idx-100]-Vm)/(Vmax-Vm+1e-6)*290;
+        cout << "sit:" << sit;
+        ui->label_6->setText(QString::number(vec[cur_idx][idx-100], 'f', 3));
+        ui->label_7->setText(QString::number((idx-100)/800*Stime, 'f', 3));
+        aimer[1]->setLine(event->x()-100, sit, event->x()-100, sit+1);
+        aimer[0]->setLine(0, sit, 10000, sit);
+    }
+
+}
+
+void Wave::on_pushButton_3_clicked() {
+    cur = !cur;
+    aimer[0]->setVisible(cur);
+    aimer[1]->setVisible(cur);
+    aimer[2]->setVisible(cur);
+    ui->comboBox->clear();
+    if(cur) {
+        for(int i = 0; i < siz; i++){
+            ui->comboBox->addItem("LINE"+QString::number(i));
+        }
+    }
+    qDebug() << cur;
+}
+
+
+void Wave::on_comboBox_currentIndexChanged(int index)
+{
+    cur_idx = index;
 }
--- stu.cpp
+++ stu.cpp
@@ -1,25 +1,6 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-
-string arr[5][4];
-vector<pair<string, string> > vec;
-
-void init(){
-    vec.emplace_back({"12", "34"});
-    vec.emplace_back({"13", "24"});
-    vec.emplace_back({"14", "23"});
-}
-
-void gogo(){
-   
-}
-
-void find_sol(){
-    gogo();
-
-}
-
 int main(){
-    init();
-    find_sol();
+    cout << INT_MAX << endl;
+    return 0;
 }
