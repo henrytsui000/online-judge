337aa1d52d587fb653ceeb8b3ee64668804f5b13
henrytsui000 <henrytsui000@gmail.com>
2022-05-19 21:33:43 +0800

    fix: Testing
    BREAKING CHANGE:

--- stu.cpp
+++ stu.cpp
@@ -1,25 +1,8 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
-#define sz(a) ((int)a.size())
-string arr[5][4];
-vector<pair<string, string> > vec;
-
-void init(){
-    vec.emplace_back({"12", "34"});
-    vec.emplace_back({"13", "24"});
-    vec.emplace_back({"14", "23"});
-}
-
-void gogo(){
-   
-}
-
-void find_sol(){
-    gogo();
-
-}
 
 int main(){
-    cout << INT_MAX << endl;
+    cout << sqrt(16)<< endl;
     return 0;
 }
